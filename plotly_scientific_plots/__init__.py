79cd3f713b3dcbb830d8fb16735e53a7228794c8
rsandler <rsandler00@gmail.com>
2019-04-25 19:42:50 +0000

    derequired pandas

--- plotly_scientific_plots/__init__.py
+++ plotly_scientific_plots/__init__.py
@@ -1,10 +1,14 @@
+
 from plotly_scientific_plots.plotly_misc import *
 from plotly_scientific_plots.plotly_plot_tools import *
 from plotly_scientific_plots.plotly_ML import *
-from plotly_scientific_plots.plotly_pandas import *
 from plotly_scientific_plots.misc_computational_tools import *
 from plotly_scientific_plots.dash_tools import *
 from plotly_scientific_plots.plot_subcomponents import *
+
+import sys
+if 'pandas' in sys.modules:
+    from plotly_scientific_plots.plotly_pandas import *
 
 # this command enables visualizations in jupyter notebook
 if in_notebook():
